4d86f8b4d8a65f3c357be5831ab9ff2db0ba88a2
Lennardj <lennardvincentjohn@gmail.com>
2022-12-20 20:14:58 +1300

    ppt

--- mainApi/index.js
+++ mainApi/index.js
@@ -1,5 +1,6 @@
 module.exports = async function (context, req) {
   context.log("JavaScript HTTP trigger function processed a request.");
+  
 
   const name = req.query.name || (req.body && req.body.name);
   const responseMessage = name
